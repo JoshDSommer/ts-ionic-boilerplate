700942e9b8384f154274bb597f3da26c2582ef4f
TheOriginalJosh <theoriginaljosh@gmail.com>
2015-12-01 11:47:29 -0500

    using injection best practice

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-  .run(function($ionicPlatform) {
+  .run(['$ionicPlatform',function($ionicPlatform) {
     $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -15,4 +15,4 @@ angular.module('starter', ['ionic'])
     //     //   StatusBar.styleDefault();
     //   }
     });
-  });
+  }]);
