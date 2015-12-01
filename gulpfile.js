96b94d07f73fc01d2ba4cc508418ff4ab54d803f
TheOriginalJosh <theoriginaljosh@gmail.com>
2015-12-01 09:56:06 -0500

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var ts = require('gulp-typescript');
+var typescript = ts.createProject('tsconfig.json');
+
+var paths = {
+  sass: ['./scss/**/*.scss'],
+  src: ['./src/*.ts']
+
+};
+
+gulp.task('default', ['sass', 'compile-ts']);
+
+gulp.task('sass', function (done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+gulp.task('compile-ts', function () {
+ var tsResult = typescript.src() // instead of gulp.src(...)
+        .pipe(ts(typescript));
+    return tsResult.js.pipe(gulp.dest(''));
+})
+gulp.task('watch', function () {
+  gulp.watch(paths.sass, ['sass']);
+  //tells gulp to watch the src directory for changes then compile.
+  gulp.watch(paths.src, ['compile-ts']);
+});
+
+gulp.task('install', ['git-check'], function () {
+  return bower.commands.install()
+    .on('log', function (data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function (done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+      );
+    process.exit(1);
+  }
+  done();
+});
